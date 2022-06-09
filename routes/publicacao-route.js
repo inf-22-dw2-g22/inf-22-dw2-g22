ee7416be927b7a99833c2a2d864fb34be30fd33e
Rubens Vinicius <rubensviniciuscastro@gmail.com>
2022-06-09 18:14:04 +0100

    change folders

--- routes/publicacao-route.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const publiController = require('../controllers/publicacao-controller');
-
-router.post('/', publiController.create);
-router.get('/', publiController.getAll);
-router.get('/:id', publiController.getAllPubli);
-router.put('/:id', publiController.update);
-router.delete('/:id', publiController.del);
-module.exports = router;
--- routes/venda-route.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const vendaController = require('../controllers/venda-controller');
-
-router.post('/', vendaController.create);
-router.get('/', vendaController.getAll);
-router.get('/:id', vendaController.getAllVendas);
-router.put('/:id', vendaController.update);
-router.delete('/:id', vendaController.del);
-module.exports = router;
